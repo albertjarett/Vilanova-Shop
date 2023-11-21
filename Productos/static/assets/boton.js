fe480d2e0326c7e595241173806fe606d3543826
albertjarett <128996701+albertjarett@users.noreply.github.com>
2023-11-20 19:40:20 -0500

    Delete Productos directory

--- Productos/static/assets/boton.js
+++
@@ -1,9 +0,0 @@
-const whatsapp = new WhatsApp({
-    phone: "+5355089270",
-    message: "Hola, vengo desde Vilanova'Shop y  deseo comprar este producto: {productName}. El precio es {productPrice}",
-  });
-  
-  const button = document.getElementById("buyButton");
-  button.addEventListener("click", () => {
-    whatsapp.send();
-  });
